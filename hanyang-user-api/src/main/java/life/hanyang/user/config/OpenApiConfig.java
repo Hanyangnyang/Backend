0c599a1a759bd0ae6d694af65906f7f26e3ad95a
준 <kdjidkr@hanyang.ac.kr>
2026-07-20 14:59:42 +0900

    chore: 캐시에서 빌드 파일 제거 및 설정 업데이트

--- hanyang-user-api/src/main/java/life/hanyang/user/config/OpenApiConfig.java
+++ hanyang-user-api/src/main/java/life/hanyang/user/config/OpenApiConfig.java
@@ -1,12 +1,17 @@
 package life.hanyang.user.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "BearerAuth";
 
     @Bean
     public OpenAPI openAPI() {
@@ -14,6 +19,13 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("하냥냥 백엔드 API 명세서")
                         .description("하냥냥 백엔드 API 명세서입니다.")
-                        .version("1.0.0"));
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                .name(SECURITY_SCHEME_NAME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
